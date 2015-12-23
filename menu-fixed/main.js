c644c5830b2f03461869a811eeea2ebc178353f3
VictorSilent <joaovpbarbosa@gmail.com>
2015-12-23 13:46:31 -0300

    Adição dos comentários nos arquivos

--- menu-fixed/main.js
+++ menu-fixed/main.js
@@ -1,14 +1,16 @@
 
 $(document).ready(function(){
-	var top1 = $('.top').offset().top;
-	$(".top").wrap('<div class="placeholder"></div>');
-	$('.placeholder').height($('.top').outerHeight());
 
-	$(window).scroll(function(){
-		var scrollPos = $(window).scrollTop();
-		if(scrollPos >= top1){
-		  $(".top").addClass('fixed');
-		}else{
+	var top1 = $('.top').offset().top;//Pega o valor da altura da div
+
+	$(".top").wrap('<div class="placeholder"></div>');//Encapsula a div dentro de outra div de wrap
+	$('.placeholder').height($('.top').outerHeight());//Atribui o mesmo height a essa nova div
+
+	$(window).scroll(function(){//Ao detectar o scroll
+		var scrollPos = $(window).scrollTop();//A variavel scroll recebe a posição atual do scroll
+		if(scrollPos >= top1){//Se a posição for maior/igual a da div
+		  $(".top").addClass('fixed');//Adiciona a classe fixed
+		}else{//Caso contrário ele remove
 		   $(".top").removeClass('fixed');
 		}
 	});
